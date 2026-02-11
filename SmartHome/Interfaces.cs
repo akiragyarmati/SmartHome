453efc6186a666bf9965328f02d4d089631345fe
akiragyarmati <akira.gyarmati@gmail.com>
2026-02-11 10:43:19 +0100

    interfaces, smartdevices

---
+++ SmartHome/Interfaces.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+    public interface ISwitchable
+    {
+        void TurnOn();
+        void TurnOff();
+    }
+
+    public interface ISensor
+    {
+        double GetData();
+        void Calibrate();
+    }
+
+
+}
---
+++ SmartHome/SmartDevices.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+    public abstract class SmartDevice : IComparable<SmartDevice>
+    {
+
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public bool IsOnline { get; set; }
+        public DateTime LastActive { get; set; }
+
+        protected SmartDevice(int id, string name)
+        {
+            Id = id;
+            Name = name;
+            IsOnline = true;
+            LastActive = DateTime.MinValue;
+        }
+
+
+        public abstract string GetStatus();
+
+
+
+        public int CompareTo(SmartDevice? other)
+        {
+            if (other == null) return 1;
+            return this.Id.CompareTo(other.Id);
+        }
+
+        public override string? ToString()
+        {
+            return $"Device ID: {Id}, Name: {Name}, Online: {IsOnline}, Last Active: {LastActive}";
+        }
+    }
+}
