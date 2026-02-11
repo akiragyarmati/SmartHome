79b5c997b6e72c67fbc5e8f0dfc2721571aa5cc9
akiragyarmati <akira.gyarmati@gmail.com>
2026-02-11 12:29:29 +0100

    SmartThermostat 1.0

--- SmartHome/Devices.cs
+++ SmartHome/Devices.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -48,5 +49,64 @@ namespace SmartHome
     }
 
 
-    
+    public class SmartThermostat : SmartDevice, ISensor
+    {
+        public double CurrentTemperature { get; private set; }
+
+        public double TargetTemperature { get; private set; } = 22.0;
+
+
+        public SmartThermostat(int id, string name, double currentTemprature) : base(id, name)
+        {
+            CurrentTemperature = currentTemprature;
+        }
+
+        public double GetData()
+        {
+            if (!IsOnline)
+            {
+                Console.WriteLine($"Hiba a/az {Name} offline!");
+                return 0.0;
+            }
+
+            LastActive = DateTime.Now;
+            return CurrentTemperature;
+        
+        }
+
+        public void Calibrate()
+        {
+            if (!IsOnline) return;
+            Console.WriteLine($"{Name} kalibrálása folyamatban");
+            LastActive = DateTime.Now;
+        }
+
+        public void SetTargetTemperature(double target)
+        {
+            if (!IsOnline)
+            {
+                Console.WriteLine($"Hiba a/az {Name} offline!");
+                return;
+            }
+
+            TargetTemperature = target;
+        }
+
+        public void SetCurrentTemperature(double temp)
+        {
+            if (!IsOnline)
+            {
+                Console.WriteLine($"Hiba a/az {Name} offline!");
+                return;
+            }
+
+            CurrentTemperature = temp;
+        }
+
+        public override string GetStatus()
+        {
+            string active = CurrentTemperature < TargetTemperature ? "Fűt" : "Készenlét";
+            return $"Hőmérséklet: {CurrentTemperature} fok, (Cél: {TargetTemperature} fok) -> {active}";
+        }
+    }
 }
