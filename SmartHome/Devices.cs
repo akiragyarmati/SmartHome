408877a87434bcf1e930d754ae84ba7671a08d07
akiragyarmati <akira.gyarmati@gmail.com>
2026-02-11 11:58:24 +0100

    SmartLight 1.0

---
+++ SmartHome/Devices.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+    public class SmartLight : SmartDevice, ISwitchable
+    {
+        public int Brightness { get; set; }
+        public SmartLight(int id, string name) : base(id, name)
+        {
+            Brightness = 0;
+        }
+        
+        public void TurnOn()
+        {
+
+            if (!IsOnline)
+            {
+                Console.WriteLine($"Hiba a/az {Name} offline!");
+                return;
+            }
+
+
+            Brightness = 100;
+            LastActive = DateTime.Now;
+        }
+
+        public void TurnOff()
+        {
+            if (!IsOnline)
+            {
+                Console.WriteLine($"Hiba a/az {Name} offline!");
+                return;
+            }
+
+            Brightness = 0;
+            LastActive = DateTime.Now;
+        }
+
+        public override string GetStatus()
+        {
+            return $"Lámpa fényereje: {Brightness}%";
+        }
+
+    }
+
+
+    
+}
