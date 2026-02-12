3c7f7a447ca5c5190623e7edc390af7b91ba72c6
akiragyarmati <akira.gyarmati@gmail.com>
2026-02-12 11:32:20 +0100

    mainben tesztelés

--- SmartHome/Program.cs
+++ SmartHome/Program.cs
@@ -8,49 +8,88 @@
 
             DeviceManager manager = new DeviceManager();
 
+            //try
+            //{ 
+            //    SmartLight nappaliLampa = new SmartLight(1,"Nappali Lámpa");
+            //    SmartThermostat furdoszobaThermo = new SmartThermostat(2,"Fürdőszoba Termosztát", 20.5);
+
+            //    nappaliLampa.Connect();
+            //    furdoszobaThermo.Connect();
+
+            //    manager.AddDevice(nappaliLampa);
+            //    manager.AddDevice(furdoszobaThermo);
+
+            //    Console.WriteLine("Vezérlés teszt:");
+
+            //    nappaliLampa.TurnOn();
+            //    Console.WriteLine(nappaliLampa.GetStatus());
+
+            //    furdoszobaThermo.SetTargetTemperature(24.0);
+            //    Console.WriteLine(furdoszobaThermo.GetStatus());
+
+            //    SmartBlinds redony = new SmartBlinds(3, "Hálószoba redőny");
+            //    redony.Connect();
+            //    manager.AddDevice(redony);
+            //    redony.SetLevel(60);
+
+            //    MusicHub hangszoro = new MusicHub(4, "Nappali Hnagszóró");
+            //    hangszoro.Connect();
+            //    manager.AddDevice(hangszoro);
+
+            //    Song zene = new Song("ahogy elképzeltem", "30Y", 3.5);
+
+            //    hangszoro.PlayMusic(zene);
+
+            //    manager.ListAllDevices();
+
+
+            //}
+            //catch (Exception ex)
+            //{
+            //    Console.WriteLine($"Hiba történt: {ex.Message}");
+            //}
+
+
+            SystemLogger logger = new SystemLogger();
+
+            logger.LogEvent += logger.LogToConsole;
+            logger.LogEvent += logger.LogToFile;
+            logger.Log("Rendszerindítás");
+
+            SmartHomeDatabase db = new SmartHomeDatabase("SmartHomeData");
+
             try
-            { 
-                SmartLight nappaliLampa = new SmartLight(1,"Nappali Lámpa");
-                SmartThermostat furdoszobaThermo = new SmartThermostat(2,"Fürdőszoba Termosztát", 20.5);
+            {
 
-                nappaliLampa.Connect();
-                furdoszobaThermo.Connect();
+                SmartLight konyhaLampa = new SmartLight(5, "Konyha Lámpa");
+                db.SaveDevice(konyhaLampa);
+                manager.AddDevice(konyhaLampa);
+                manager.ShowDeviceCapabilities(konyhaLampa);
 
-                manager.AddDevice(nappaliLampa);
-                manager.AddDevice(furdoszobaThermo);
+                SmartThermostat futes = new SmartThermostat(6, "Nappali Fűtés", 22.0);
+                db.SaveDevice(futes);
+                manager.AddDevice(futes);
+                manager.ShowDeviceCapabilities(futes);
 
-                Console.WriteLine("Vezérlés teszt:");
-
-                nappaliLampa.TurnOn();
-                Console.WriteLine(nappaliLampa.GetStatus());
-
-                furdoszobaThermo.SetTargetTemperature(24.0);
-                Console.WriteLine(furdoszobaThermo.GetStatus());
-
-                SmartBlinds redony = new SmartBlinds(3, "Hálószoba redőny");
-                redony.Connect();
+                SmartBlinds redony = new SmartBlinds(7, "Hálószoba Redőny");
+                db.SaveDevice(redony);
                 manager.AddDevice(redony);
-                redony.SetLevel(60);
+                manager.ShowDeviceCapabilities(redony);
 
-                MusicHub hangszoro = new MusicHub(4, "Nappali Hnagszóró");
-                hangszoro.Connect();
+                MusicHub hangszoro = new MusicHub(8, "Nappali Hnagszóró");
+                db.SaveDevice(hangszoro);
                 manager.AddDevice(hangszoro);
+                manager.ShowDeviceCapabilities(hangszoro);
 
-                Song zene = new Song("ahogy elképzeltem", "30Y", 3.5);
-
-                hangszoro.PlayMusic(zene);
-
-                manager.ListAllDevices();
+                logger.Log("Eszközök mentése sikeres");
 
 
             }
             catch (Exception ex)
             {
-                Console.WriteLine($"Hiba történt: {ex.Message}");
+                logger.Log($"Hiba a mentés során: {ex.Message}");
+
             }
-
-
-
         }
     }
 }
