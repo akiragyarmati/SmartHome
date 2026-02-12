7c5e5d258ccf732549f70e4e16bff24b5ce84cee
akiragyarmati <akira.gyarmati@gmail.com>
2026-02-12 10:44:19 +0100

    DeviceCategoryAttr

---
+++ SmartHome/CustomAttributes.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SmartHome
+{
+
+    [AttributeUsage(AttributeTargets.Class, AllowMultiple = false)]
+    public class DeviceCategoryAttribute : Attribute
+    {
+        public string Category { get; }
+        public int SecurityLevel { get; }
+
+        public DeviceCategoryAttribute(string category, int securityLevel = 1)
+        {
+            Category = category;
+            SecurityLevel = securityLevel;
+        }
+
+    }
+}
--- SmartHome/Devices.cs
+++ SmartHome/Devices.cs
@@ -8,6 +8,7 @@ using System.Threading.Tasks;
 namespace SmartHome
 {
     //Lámpa
+    [DeviceCategory("Világítás", 2)]
     public class SmartLight : SmartDevice, ISwitchable
     {
         public int Brightness { get; set; }
@@ -50,6 +51,7 @@ namespace SmartHome
     }
 
     //Termosztát
+    [DeviceCategory("Termosztát", 3)]
     public class SmartThermostat : SmartDevice, ISensor
     {
         public double CurrentTemperature { get; private set; }
@@ -113,6 +115,7 @@ namespace SmartHome
 
 
 
+    [DeviceCategory("Redőny", 2)]
     public class SmartBlinds : SmartDevice
     { 
         public int OpenPercentage { get; private set; }
@@ -140,6 +143,7 @@ namespace SmartHome
         }
     }
 
+    [DeviceCategory("Zene", 1)]
     public class MusicHub : SmartDevice, ISwitchable
     { 
         //Tömbbegyűjetni a zenéket, mint memória, max. 5 szám
